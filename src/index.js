e59791d7635c2e9b68edf9fb3a39c43ce33406e6
jkl303 <303vladzoz@gmail.com>
2022-10-18 19:09:51 +0200

    completed

--- src/index.js
+++ src/index.js
@@ -3,38 +3,38 @@ import { fetchCountries } from './fetchCountries';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 import countryList from './countryList.hbs';
+import countryInfo from './countryInfo.hbs'
 const DEBOUNCE_DELAY = 300;
 const input = document.querySelector('#search-box');
 const list = document.querySelector('.country-list');
 const info = document.querySelector('.country-info');
+function clearMarkup() {
+  list.innerHTML = '';
+  info.innerHTML = '';
+}
 input.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
 function searchCountry() {
   if (input.value.trim() === '') {
-    list.innerHTML = '';
-    info.innerHTML = '';
+    clearMarkup();
   } else {
     fetchCountries(input.value.trim()).then(countries => {
-    
       if (countries.length > 10) {
         Notify.info("Too many matches found. Please enter a more specific name.")
+        clearMarkup();
       }
       if (countries.length > 1 && countries.length <= 10) {
-        console.log(countries[0].name.common);
-        list.insertAdjacentHTML('beforeend', countryList(countries))
-      } else {
+        list.insertAdjacentHTML('beforeend', countryList(countries));
+        info.innerHTML = '';
+
+      }
+      if (countries.length === 1) {
+        info.innerHTML = countryInfo(countries[0]);
+        list.innerHTML = '';
       }
     
     }
-    ).
-      catch((error) => console.log(error));
+    ).catch((error) => {
+      Notify.failure('Oops, there is no country with that name');
+      clearMarkup(); });
   }
 }
-    // const { name, capital, population, flags, languages } = countries[0];
-    //
-    //     console.log(name.official);
-    //     console.log(capital[0]);
-    //     console.log(population);
-    //     console.log(flags.svg);
-    //     console.log(Object.values(languages));
-    //     }
-    console.log('dsfa');
